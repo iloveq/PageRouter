0c293508622d8d99a6865df4f424f65a1673699b
haoran <woaigmz@gmail.com>
2019-12-05 17:07:22 +0800

    支持 bundle 到 AppCompatActivity

--- apt-api/src/main/java/com/sohu/hy/api/BundleService.java
+++ apt-api/src/main/java/com/sohu/hy/api/BundleService.java
@@ -3,34 +3,52 @@ package com.sohu.hy.api;
 import android.app.Activity;
 
 import java.lang.reflect.Method;
-import java.util.Objects;
 
 public class BundleService {
 
     public static void bind(Activity target) {
-        bind(target, false);
-    }
-
-    public static void bind(Activity target, boolean isBindParent) {
         try {
-            String bundlePath;
-            if (isBindParent) {
-                bundlePath = "com.sohu.generate." + Objects.requireNonNull(target.getClass().getSuperclass()).getSimpleName() + "Bundle";
-            } else {
-                bundlePath = "com.sohu.generate." + target.getClass().getSimpleName() + "Bundle";
-            }
-            if (bundlePath.isEmpty()) return;
-            Class<?> c = Class.forName(bundlePath);
-            Method bind;
-            if (isBindParent) {
-                bind = c.getMethod("bind", target.getClass().getSuperclass());
-            } else {
-                bind = c.getMethod("bind", target.getClass());
-            }
-            bind.invoke(null, target);
+            boolean isContinueToBind;
+            Class currentClass = target.getClass();
+            do {
+                isContinueToBind = isContinueToBind(target, currentClass);
+                if (isContinueToBind) {
+                    assert currentClass != null;
+                    currentClass = currentClass.getSuperclass();
+                }
+            } while (isContinueToBind);
         } catch (Exception e) {
             e.printStackTrace();
         }
+    }
+
+    private static boolean isContinueToBind(Activity target, Class<? extends Activity> targetClass) {
+        try {
+            String className = targetClass.getSimpleName();
+            if (className.isEmpty()) return false;
+            if (className.equals("AppCompatActivity")) return false;
+            String bundlePath = "com.sohu.generate." + className + "Launcher";
+            Class<?> c;
+            try {
+                c = Class.forName(bundlePath);
+            }catch (Exception e){
+                c = null;
+            }
+            if(c!=null){
+                Method bind;
+                try {
+                    bind = c.getMethod("bind", targetClass);
+                }catch(Exception e){
+                    bind = null;
+                }
+                if (bind!=null){
+                    bind.invoke(null, target);
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+        return true;
     }
 
 }
